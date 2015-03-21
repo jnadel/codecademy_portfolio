a76814d3e09761316046f680649b61447e44a0e7
Jonathan Nadel <jonathan.nadel@gmail.com>
2015-03-21 13:57:50 -0700

    unformatted welcome page, unslider portfolio

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+  $(function() { 
+      $('.banner').unslider( {
+        speed: 1000,               //  The speed to animate each slide (in milliseconds)
+  	    delay: 1000,              //  The delay between slide animations (in milliseconds)
+  	    complete: function() {},  //  A function that gets called after every slide animation
+  	    keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+  	    dots: true,               //  Display dot navigation
+  	    fluid: false              //  Support responsive design. May break non-responsive designs
+      });
+  });
+
+
+});
