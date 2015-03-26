522934bed5c4746464f6fcf27724435f06072e2e
Jonathan Nadel <jonathan.nadel@gmail.com>
2015-03-25 23:36:27 -0700

    slider present with error, contact view created

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,15 +1,41 @@
 $(document).ready(function () {
 
-  $(function() { 
-      $('.banner').unslider( {
-        speed: 1000,               //  The speed to animate each slide (in milliseconds)
-  	    delay: 1000,              //  The delay between slide animations (in milliseconds)
-  	    complete: function() {},  //  A function that gets called after every slide animation
-  	    keys: true,               //  Enable keyboard (left, right) arrow shortcuts
-  	    dots: true,               //  Display dot navigation
-  	    fluid: false              //  Support responsive design. May break non-responsive designs
-      });
-  });
+  // $(function() {
+  //     $('.banner').unslider( {
+  //       speed: 1000,               //  The speed to animate each slide (in milliseconds)
+  // 	    delay: 1000,              //  The delay between slide animations (in milliseconds)
+  // 	    complete: function() {},  //  A function that gets called after every slide animation
+  // 	    keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+  // 	    dots: true,               //  Display dot navigation
+  // 	    fluid: false              //  Support responsive design. May break non-responsive designs
+  //     });
+  // });
 
 
+});
+
+
+
+$(document).ready(function(){
+    // Activate carousel
+    $("#myCarousel").carousel();
+
+    // Enable carousel control
+	$(".left").click(function(){
+    	$("#myCarousel").carousel('prev');
+    });
+    $(".right").click(function(){
+    	$("#myCarousel").carousel('next');
+    });
+
+    // Enable carousel indicators
+    $(".slide-one").click(function(){
+    	$("#myCarousel").carousel(0);
+    });
+    $(".slide-two").click(function(){
+    	$("#myCarousel").carousel(1);
+    });
+    $(".slide-three").click(function(){
+    	$("#myCarousel").carousel(2);
+    });
 });
